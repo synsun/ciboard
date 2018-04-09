44755664340c6251703b1eba36bea6e7974cd34f
synsun <synsun1202@gmail.com>
2018-04-09 16:48:40 +0900

    CodeIgniter Version 3.1.8 update

--- _system/libraries/Session/drivers/Session_files_driver.php
+++ _system/libraries/Session/drivers/Session_files_driver.php
@@ -6,7 +6,7 @@
  *
  * This content is released under the MIT License (MIT)
  *
- * Copyright (c) 2014 - 2017, British Columbia Institute of Technology
+ * Copyright (c) 2014 - 2018, British Columbia Institute of Technology
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -29,7 +29,7 @@
  * @package	CodeIgniter
  * @author	EllisLab Dev Team
  * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
- * @copyright	Copyright (c) 2014 - 2017, British Columbia Institute of Technology (http://bcit.ca/)
+ * @copyright	Copyright (c) 2014 - 2018, British Columbia Institute of Technology (http://bcit.ca/)
  * @license	http://opensource.org/licenses/MIT	MIT License
  * @link	https://codeigniter.com
  * @since	Version 3.0.0
@@ -111,11 +111,6 @@ class CI_Session_files_driver extends CI_Session_driver implements SessionHandle
 		{
 			log_message('debug', 'Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.');
 			$this->_config['save_path'] = rtrim(ini_get('session.save_path'), '/\\');
-			if( ! $this->_config['save_path'])
-			{
-				$this->_config['save_path'] = '/tmp';
-				ini_set('session.save_path', $this->_config['save_path']);
-			}
 		}
 
 		$this->_sid_regexp = $this->_config['_sid_regexp'];
